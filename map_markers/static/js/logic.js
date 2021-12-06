7b3084a0ef11d46422f3a8df5c434712b7648c1d
Sangee_uofa <reachme1212@gmail.com>
2021-12-06 12:36:20 -0700

    added map marker

--- map_markers/static/js/logic.js
+++ map_markers/static/js/logic.js
@@ -4,6 +4,16 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.circleMarker([34.0522, -118.2437], {
+    radius: 100,
+    fillColor: "#ffffa1",
+    fillOpacity: 0.5,
+    color: "black"
+
+
+}).addTo(map);
+
 // Create the map object with a center and zoom level.
 // let map = L.map("mapid", {
 //     center: [
@@ -13,9 +23,9 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 // });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox://styles/mapbox/outdoors-v11',
+    id: 'mapbox://styles/mapbox/dark-v10',
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
