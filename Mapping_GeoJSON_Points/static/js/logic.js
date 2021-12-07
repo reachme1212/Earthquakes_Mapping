cd239852df2de35e3373ae628d2ef7c0f25410a7
Sangee_uofa <reachme1212@gmail.com>
2021-12-07 15:09:25 -0700

    added airportjson

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -33,35 +33,35 @@ let sanFranAirport = {
 // Grabbing our GeoJSON data.
 // Grabbing our GeoJSON data.
 L.geoJSON(sanFranAirport, {
-            // We turn each feature into a marker on the map.
-            pointToLayer: function(feature, latlng) {
-                console.log(feature);
-                return L.marker(latlng).bindPopup("<h2>" +
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+        console.log(feature);
+        return L.marker(latlng).bindPopup("<h2>" +
 
-                    feature.properties.faa + "</h2> <hr> <h3>" + feature.properties.name + "</h3>  ");
-            }
+            feature.properties.faa + "</h2> <hr> <h3>" + feature.properties.name + "</h3>  ");
+    }
 
-            // }).addTo(map);
+}).addTo(map);
 
-            // L.geoJSON(sanFranAirport, {
-            //     onEachFeature: function(feature, layer) {
-            //         console.log(layer);
-            //         layer.bindPopup();
-            //     }
-            // }).addTo(map);
-
-
+// L.geoJSON(sanFranAirport, {
+//     onEachFeature: function(feature, layer) {
+//         console.log(layer);
+//         layer.bindPopup();
+//     }
+// }).addTo(map);
 
 
 
-            // We create the tile layer that will be the background of our map.
-                let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                id: 'mapbox/satellite-streets-v11',
-                tileSize: 512,
-                maxZoom: 18,
-                zoomOffset: -1,
-                accessToken: API_KEY
-            });
-            // Then we add our 'graymap' tile layer to the map.
-            streets.addTo(map);
+
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/satellite-streets-v11',
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
