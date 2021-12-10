31547803c3a147094762fd8e0ea115ec48d88265
Sangee_uofa <reachme1212@gmail.com>
2021-12-09 19:37:28 -0700

    corrected index

--- static/js/tectonic_plate_logic.js
+++ static/js/tectonic_plate_logic.js
@@ -178,7 +178,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         const magnitudes = [0, 1, 2, 3, 4, 5];
         const colors = [
             "lightslategray",
-            "#pink",
+            "pink",
             "lightskyblue",
             "yellow",
             "orange",
